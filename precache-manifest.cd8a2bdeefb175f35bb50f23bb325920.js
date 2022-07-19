a2879ca6ecb0e4f4dba6382c433d7628bdb959ab
abdelkaderkasmi <ak.kasmi@gmail.com>
2022-07-19 17:44:24 +0200

    Updates

---
+++ precache-manifest.cd8a2bdeefb175f35bb50f23bb325920.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0b313056250edd51c13cbfce5658d39",
+    "url": "/TP3/index.html"
+  },
+  {
+    "revision": "f4272b9d354e7b8ee989",
+    "url": "/TP3/static/css/main.b9762e37.chunk.css"
+  },
+  {
+    "revision": "7c389947f751b34441fc",
+    "url": "/TP3/static/js/2.43f4e349.chunk.js"
+  },
+  {
+    "revision": "f4272b9d354e7b8ee989",
+    "url": "/TP3/static/js/main.ed8b63b6.chunk.js"
+  },
+  {
+    "revision": "9eb26b3e3b57ec70aece",
+    "url": "/TP3/static/js/runtime-main.22a8638a.js"
+  }
+]);
